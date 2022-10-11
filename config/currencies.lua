2bd6d045bacc87be61d3251404ff9f422fa064f0
AsKETbest <lenyh54@gmail.com>
2022-10-11 20:51:55 +0300

    Update res

--- config/currencies.lua
+++ config/currencies.lua
@@ -11,10 +11,6 @@ local function addCurrency(name, id, dmg, model, color, max)
 end
 
 addCurrency("Деньги",           "customnpcs:npcMoney",      0,   'INGOT', 0x85BB65, nil)
-addCurrency("Железный слиток",  "minecraft:iron_ingot",     0,   'INGOT', 0xAAAAAA, nil)
-addCurrency("Железный блок",    "minecraft:iron_block",     0,   'BLOCK', 0xAAAAAA, 6)
-addCurrency("Медный слиток",    "IC2:itemIngot",            0,   'INGOT', 0xA5642F, nil)
-addCurrency("Медный блок",      "Forestry:resourceStorage", 1,   'BLOCK', 0xA5642F, 6)
 addCurrency("Бесплатно",        nil,                        nil, nil,     0xE5E5E5, nil)
 
 return currencies
